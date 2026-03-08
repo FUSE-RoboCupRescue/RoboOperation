5f29996d690d92f84314d1ec2973e5427858877f
Eckart Cobo-Briesewitz <68149355+Tupryk@users.noreply.github.com>
2026-03-08 21:26:54 +0100

    Add XPiperController for arm and gripper control

--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,8 @@
+import rclpy
+from rclpy.node import Node
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+import time
+
 BUTTON_X = 0
 BUTTON_A = 1
 BUTTON_B = 2
@@ -10,10 +15,16 @@ JOY_LEFT_X = 0
 JOY_LEFT_Y = 1
 JOY_RIGHT_X = 2
 JOY_RIGHT_Y = 3
+# This needs to be checked...
+PAD_UP = 4
+PAD_RIGHT = 5
+PAD_DOWN = 6
+PAD_LEFT = 7
 
 MAX_FLIPPER_VEL = 40
 MAX_LINEAR_VEL = 50
 MAX_ANGULAR_VEL = 50  # Should be in radians but stfu
+
 
 def joy2string(buttons, axes):
     # This is just for the teensy!!!
@@ -55,5 +66,74 @@ def joy2string(buttons, axes):
     out = f"{track_right} {track_left} {-flipper_front} {flipper_front} {flipper_back} {-flipper_back}"
     return out
 
-def control_arm(buttons, axes):
-    pass
+
+class XPiperController(Node):
+
+    def __init__(self):
+        super().__init__('xpiper_controller')
+
+        # Arm trajectory publisher
+        self.arm_pub = self.create_publisher(
+            JointTrajectory,
+            '/xpiper_arm_controller/joint_trajectory',
+            10
+        )
+
+        # Gripper trajectory publisher
+        self.gripper_pub = self.create_publisher(
+            JointTrajectory,
+            '/xpiper_gripper_controller/joint_trajectory',
+            10
+        )
+
+        self.joint_step = 0.1
+        self.current_joint_state = [0., 0., 0., 0., 0., 0.]
+        self.gripper_state = False  # False: closed; True: open
+
+    def move(self, buttons, axes):
+
+        if buttons[BUTTON_RT]:  # Gripper action
+            msg = JointTrajectory()
+            msg.joint_names = ['gripper_joint']
+
+            point = JointTrajectoryPoint()
+            point.positions = [0.0] if self.gripper_state else [0.04]
+            self.gripper_state = not self.gripper_state
+
+            point.time_from_start.sec = 1
+
+            msg.points.append(point)
+
+            self.gripper_pub.publish(msg)
+
+            time.sleep(2)
+
+        else:  # Arm action
+            msg = JointTrajectory()
+
+            msg.joint_names = [
+                'joint1',
+                'joint2',
+                'joint3',
+                'joint4',
+                'joint5',
+                'joint6'
+            ]
+            duration = 1
+
+            self.current_joint_state[0] += self.joint_step * axes[JOY_RIGHT_X]
+            self.current_joint_state[1] += self.joint_step * axes[JOY_RIGHT_Y]
+            self.current_joint_state[2] += self.joint_step * axes[JOY_LEFT_X]
+            self.current_joint_state[3] += self.joint_step * axes[JOY_LEFT_X]
+            self.current_joint_state[4] += self.joint_step * axes[PAD_UP]
+            self.current_joint_state[5] += self.joint_step * axes[PAD_RIGHT]
+
+            point = JointTrajectoryPoint()
+            point.positions = self.current_joint_state
+            point.time_from_start.sec = duration
+
+            msg.points.append(point)
+
+            self.arm_pub.publish(msg)
+
+            time.sleep(duration + 1)
