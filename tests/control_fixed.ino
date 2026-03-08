69dac2ac73fb29321dd4205593608fef60f38240
Eckart Cobo Briesewitz <eck4art@gmail.com>
2026-03-08 18:13:40 +0100

    limit switches

--- tests/control_fixed.ino
+++ tests/control_fixed.ino
@@ -1,72 +1,83 @@
-#define ML_FB 20
-#define ML_BRK 19
-#define ML_SPD 33
-
+// Tracks Right
 #define MR_FB 39
 #define MR_BRK 37
 #define MR_SPD 15
 
-#define F1_FB 12
-#define F1_BRK 24
-#define F1_SPD 3
+// Tracks Left
+#define ML_FB 20
+#define ML_BRK 19
+#define ML_SPD 33
 
-#define F2_FB 9
-#define F2_BRK 10
-#define F2_SPD 2
+// Flipper Front Right
+#define FFR_FB 9
+#define FFR_BRK 10
+#define FFR_SPD 2
+#define FFR_LS 29
 
-#define F3_FB 30
-#define F3_BRK 31
-#define F3_SPD 4
+// Flipper Front Left
+#define FFL_FB 30
+#define FFL_BRK 31
+#define FFL_SPD 4
+#define FFL_LS 26
 
-#define F4_FB 40
-#define F4_BRK 41
-#define F4_SPD 14
+// Flipper Back Right
+#define FBR_FB 12
+#define FBR_BRK 24
+#define FBR_SPD 3
+#define FBR_LS 28
+
+// Flipper Back Left
+#define FBL_FB 40
+#define FBL_BRK 41
+#define FBL_SPD 14
+#define FBL_LS 27
 
 
-void setup() {
-    Serial.begin(115200);
-    while (!Serial);
-
-    // motor l
-    pinMode(ML_FB, OUTPUT); //FB
-    pinMode(ML_BRK, OUTPUT); //break
-    analogWriteFrequency(ML_SPD, 1000);
-
-    // motor r
+void setup()
+{
     pinMode(MR_FB, OUTPUT);
-    pinMode(MR_BRK, OUTPUT);
+    pinMode(MR_BRK, OUTPUT); // break
     analogWriteFrequency(MR_SPD, 1000);
 
-    // flipper 1
-    pinMode(F1_FB, OUTPUT);
-    pinMode(F1_BRK, OUTPUT);
-    analogWriteFrequency(F1_FB, 1000);
+    pinMode(ML_FB, OUTPUT);
+    pinMode(ML_BRK, OUTPUT);
+    analogWriteFrequency(ML_SPD, 1000);
 
-    // flipper 2
-    pinMode(F2_FB, OUTPUT);
-    pinMode(F2_BRK, OUTPUT);
-    analogWriteFrequency(F2_FB, 1000);
+    pinMode(FFR_FB, OUTPUT);
+    pinMode(FFR_BRK, OUTPUT);
+    analogWriteFrequency(FFR_FB, 1000);
+    pinMode(FFR_LS, INPUT);
 
-    // flipper 3
-    pinMode(F3_FB, OUTPUT);
-    pinMode(F3_BRK, OUTPUT);
-    analogWriteFrequency(F3_FB, 1000);
+    pinMode(FFL_FB, OUTPUT);
+    pinMode(FFL_BRK, OUTPUT);
+    analogWriteFrequency(FFL_FB, 1000);
+    pinMode(FFL_LS, INPUT);
 
-    // flipper 4
-    pinMode(F4_FB, OUTPUT);
-    pinMode(F4_BRK, OUTPUT);
-    analogWriteFrequency(F4_FB, 1000);
+    pinMode(FBR_FB, OUTPUT);
+    pinMode(FBR_BRK, OUTPUT);
+    analogWriteFrequency(FBR_FB, 1000);
+    pinMode(FBR_LS, INPUT);
+
+    pinMode(FBL_FB, OUTPUT);
+    pinMode(FBL_BRK, OUTPUT);
+    analogWriteFrequency(FBL_FB, 1000);
+    pinMode(FBL_LS, INPUT);
 
     driveMR(0);
     driveML(0);
-    driveF1(0);
-    driveF2(0);
-    driveF3(0);
-    driveF4(0);
+    driveFFR(0);
+    driveFFL(0);
+    driveFBR(0);
+    driveFBL(0);
+
+    Serial.begin(115200);
+    while (!Serial);
 }
 
-void loop() {
-    if (Serial.available() > 0) {
+void loop()
+{
+    if (Serial.available() > 0)
+    {
         String incoming = Serial.readStringUntil('\n');
         incoming.trim();
 
@@ -74,123 +85,175 @@ void loop() {
         int count = 0;
         String temp = incoming;
 
-        while (temp.length() > 0 && count < 6) {
+        while (temp.length() > 0 && count < 6)
+        {
             int idx = temp.indexOf(' ');
             String token = (idx == -1) ? temp : temp.substring(0, idx);
             velocity[count++] = token.toInt();
-            if (idx == -1) break;
+            if (idx == -1)
+                break;
             temp = temp.substring(idx + 1);
         }
 
         driveMR(velocity[0]);
         driveML(velocity[1]);
-        driveF1(velocity[4]);
-        driveF2(velocity[2]);
-        driveF3(velocity[3]);
-        driveF4(velocity[5]);
+        
+        if (digitalRead(FFR_LS)) driveFFR(0);
+        else driveFFR(velocity[2]);
 
-        for (int i = 0; i < count; i++) {
+        if (digitalRead(FFL_LS)) driveFFL(0);
+        else driveFFL(velocity[3]);
+
+        if (digitalRead(FBR_LS)) driveFBR(0);
+        else driveFBR(velocity[4]);
+
+        if (digitalRead(FBL_LS)) driveFBL(0);
+        else driveFBL(velocity[5]);
+        
+        for (int i = 0; i < count; i++)
+        {
             Serial.print("velocity ");
             Serial.print(i + 1);
             Serial.print(": ");
-            Serial.print(velocity[i]);;
-            Serial.print(";");  // explicit carriage return + newline
+            Serial.print(velocity[i]);
+            ;
+            Serial.print(";"); // explicit carriage return + newline
         }
+    }
+    if (digitalRead(FFR_LS)) driveFFR(0);
+    if (digitalRead(FFL_LS)) driveFFL(0);
+    if (digitalRead(FBR_LS)) driveFBR(0);
+    if (digitalRead(FBL_LS)) driveFBL(0);
+}
+
+void driveML(int spd)
+{
+    if (spd > 0)
+    {
+        analogWrite(ML_SPD, 255 - spd); // needs to be remap to 255-0 (255 is stop 0 is max)
+        digitalWrite(ML_BRK, 0);
+        digitalWrite(ML_FB, 0);
+    }
+    else if (spd < 0)
+    {
+        analogWrite(ML_SPD, 255 + spd);
+        digitalWrite(ML_BRK, 0);
+        digitalWrite(ML_FB, 1);
+    }
+    else
+    {
+        analogWrite(ML_SPD, 255);
+        digitalWrite(ML_BRK, 1);
+        digitalWrite(ML_FB, 0);
     }
 }
 
-void driveML(int spd) {
-  if (spd > 0) {
-    analogWrite(ML_SPD, 255 - spd); // needs to be remap to 255-0 (255 is stop 0 is max)
-    digitalWrite(ML_BRK, 0);
-    digitalWrite(ML_FB, 0);
-  } else if (spd < 0) {
-    analogWrite(ML_SPD, 255 + spd);
-    digitalWrite(ML_BRK, 0);
-    digitalWrite(ML_FB, 1);
-  } else {
-    analogWrite(ML_SPD, 255);
-    digitalWrite(ML_BRK, 1);
-    digitalWrite(ML_FB, 0);
-  }
+void driveMR(int spd)
+{
+    if (spd > 0)
+    {
+        analogWrite(MR_SPD, 255 - spd);
+        digitalWrite(MR_BRK, 0);
+        digitalWrite(MR_FB, 0);
+    }
+    else if (spd < 0)
+    {
+        analogWrite(MR_SPD, 255 + spd);
+        digitalWrite(MR_BRK, 0);
+        digitalWrite(MR_FB, 1);
+    }
+    else
+    {
+        analogWrite(MR_SPD, 255);
+        digitalWrite(MR_BRK, 1);
+        digitalWrite(MR_FB, 0);
+    }
 }
 
-void driveMR(int spd) {
-  if (spd > 0) {
-    analogWrite(MR_SPD, 255 - spd);
-    digitalWrite(MR_BRK, 0);
-    digitalWrite(MR_FB, 0);
-  } else if (spd < 0) {
-    analogWrite(MR_SPD, 255 + spd);
-    digitalWrite(MR_BRK, 0);
-    digitalWrite(MR_FB, 1);
-  } else {
-    analogWrite(MR_SPD, 255);
-    digitalWrite(MR_BRK, 1);
-    digitalWrite(MR_FB, 0);
-  }
+void driveFBR(int spd)
+{
+    if (spd > 0)
+    {
+        analogWrite(FBR_SPD, 255 - spd);
+        digitalWrite(FBR_BRK, 0);
+        digitalWrite(FBR_FB, 0);
+    }
+    else if (spd < 0)
+    {
+        analogWrite(FBR_SPD, 255 + spd);
+        digitalWrite(FBR_BRK, 0);
+        digitalWrite(FBR_FB, 1);
+    }
+    else
+    {
+        analogWrite(FBR_SPD, 255);
+        digitalWrite(FBR_BRK, 1);
+        digitalWrite(FBR_FB, 0);
+    }
 }
 
-void driveF1(int spd) {
-  if (spd > 0) {
-    analogWrite(F1_SPD, 255 - spd);
-    digitalWrite(F1_BRK, 0);
-    digitalWrite(F1_FB, 0);
-  } else if (spd < 0) {
-    analogWrite(F1_SPD, 255 + spd);
-    digitalWrite(F1_BRK, 0);
-    digitalWrite(F1_FB, 1);
-  } else {
-    analogWrite(F1_SPD, 255);
-    digitalWrite(F1_BRK, 1);
-    digitalWrite(F1_FB, 0);
-  }
+void driveFFR(int spd)
+{
+    if (spd > 0)
+    {
+        analogWrite(FFR_SPD, 255 - spd);
+        digitalWrite(FFR_BRK, 0);
+        digitalWrite(FFR_FB, 0);
+    }
+    else if (spd < 0)
+    {
+        analogWrite(FFR_SPD, 255 + spd);
+        digitalWrite(FFR_BRK, 0);
+        digitalWrite(FFR_FB, 1);
+    }
+    else
+    {
+        analogWrite(FFR_SPD, 255);
+        digitalWrite(FFR_BRK, 1);
+        digitalWrite(FFR_FB, 0);
+    }
 }
 
-void driveF2(int spd) {
-  if (spd > 0) {
-    analogWrite(F2_SPD, 255 - spd);
-    digitalWrite(F2_BRK, 0);
-    digitalWrite(F2_FB, 0);
-  } else if (spd < 0) {
-    analogWrite(F2_SPD, 255 + spd);
-    digitalWrite(F2_BRK, 0);
-    digitalWrite(F2_FB, 1);
-  } else {
-    analogWrite(F2_SPD, 255);
-    digitalWrite(F2_BRK, 1);
-    digitalWrite(F2_FB, 0);
-  }
+void driveFFL(int spd)
+{
+    if (spd > 0)
+    {
+        analogWrite(FFL_SPD, 255 - spd);
+        digitalWrite(FFL_BRK, 0);
+        digitalWrite(FFL_FB, 0);
+    }
+    else if (spd < 0)
+    {
+        analogWrite(FFL_SPD, 255 + spd);
+        digitalWrite(FFL_BRK, 0);
+        digitalWrite(FFL_FB, 1);
+    }
+    else
+    {
+        analogWrite(FFL_SPD, 255);
+        digitalWrite(FFL_BRK, 1);
+        digitalWrite(FFL_FB, 0);
+    }
 }
 
-void driveF3(int spd) {
-  if (spd > 0) {
-    analogWrite(F3_SPD, 255 - spd);
-    digitalWrite(F3_BRK, 0);
-    digitalWrite(F3_FB, 0);
-  } else if (spd < 0) {
-    analogWrite(F3_SPD, 255 + spd);
-    digitalWrite(F3_BRK, 0);
-    digitalWrite(F3_FB, 1);
-  } else {
-    analogWrite(F3_SPD, 255);
-    digitalWrite(F3_BRK, 1);
-    digitalWrite(F3_FB, 0);
-  }
-}
-
-void driveF4(int spd) {
-  if (spd > 0) {
-    analogWrite(F4_SPD, 255 - spd);
-    digitalWrite(F4_BRK, 0);
-    digitalWrite(F4_FB, 0);
-  } else if (spd < 0) {
-    analogWrite(F4_SPD, 255 + spd);
-    digitalWrite(F4_BRK, 0);
-    digitalWrite(F4_FB, 1);
-  } else {
-    analogWrite(F4_SPD, 255);
-    digitalWrite(F4_BRK, 1);
-    digitalWrite(F4_FB, 0);
-  }
+void driveFBL(int spd)
+{
+    if (spd > 0)
+    {
+        analogWrite(FBL_SPD, 255 - spd);
+        digitalWrite(FBL_BRK, 0);
+        digitalWrite(FBL_FB, 0);
+    }
+    else if (spd < 0)
+    {
+        analogWrite(FBL_SPD, 255 + spd);
+        digitalWrite(FBL_BRK, 0);
+        digitalWrite(FBL_FB, 1);
+    }
+    else
+    {
+        analogWrite(FBL_SPD, 255);
+        digitalWrite(FBL_BRK, 1);
+        digitalWrite(FBL_FB, 0);
+    }
 }
