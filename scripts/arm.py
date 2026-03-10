4185f607feea1550d6f91a6172a3135656c0ff03
Eckart Cobo Briesewitz <eck4art@gmail.com>
2026-03-10 18:20:16 +0100

    switched to piper_sdk

--- scripts/arm.py
+++ scripts/arm.py
@@ -1,77 +1,47 @@
-import rclpy
-from rclpy.node import Node
-from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 import time
+from piper_sdk import C_PiperInterface
 from utils import *
 
 
-class XPiperController(Node):
+class XPiperController:
 
     def __init__(self):
-        super().__init__('xpiper_controller')
 
-        # Arm trajectory publisher
-        self.arm_pub = self.create_publisher(
-            JointTrajectory,
-            '/xpiper_arm_controller/joint_trajectory',
-            10
-        )
+        # Connect to Piper via CAN
+        self.piper = C_PiperInterface("can0")
+        self.piper.ConnectPort()
 
-        # Gripper trajectory publisher
-        self.gripper_pub = self.create_publisher(
-            JointTrajectory,
-            '/xpiper_gripper_controller/joint_trajectory',
-            10
-        )
+        # Enable the arm
+        self.piper.EnableArm()
 
         self.joint_step = 0.1
         self.current_joint_state = [0., 0., 0., 0., 0., 0.]
-        self.gripper_state = False  # False: closed; True: open
+        self.gripper_state = False  # False: closed, True: open
 
     def move(self, buttons, axes):
 
         if buttons[BUTTON_RT]:  # Gripper action
-            msg = JointTrajectory()
-            msg.joint_names = ['gripper_joint']
 
-            point = JointTrajectoryPoint()
-            point.positions = [0.0] if self.gripper_state else [0.04]
+            if self.gripper_state:
+                self.piper.GripperControl(0.0)     # close
+            else:
+                self.piper.GripperControl(0.04)    # open
+
             self.gripper_state = not self.gripper_state
 
-            point.time_from_start.sec = 1
-
-            msg.points.append(point)
-
-            self.gripper_pub.publish(msg)
-
-            time.sleep(2)
+            time.sleep(1)
 
         else:  # Arm action
-            msg = JointTrajectory()
-
-            msg.joint_names = [
-                'joint1',
-                'joint2',
-                'joint3',
-                'joint4',
-                'joint5',
-                'joint6'
-            ]
-            duration = 1
 
             self.current_joint_state[0] += self.joint_step * axes[JOY_RIGHT_X]
             self.current_joint_state[1] += self.joint_step * axes[JOY_RIGHT_Y]
             self.current_joint_state[2] += self.joint_step * axes[JOY_LEFT_X]
-            self.current_joint_state[3] += self.joint_step * axes[JOY_LEFT_X]
+            self.current_joint_state[3] += self.joint_step * axes[JOY_LEFT_Y]
             self.current_joint_state[4] += self.joint_step * axes[PAD_X]
             self.current_joint_state[5] += self.joint_step * axes[PAD_Y]
 
-            point = JointTrajectoryPoint()
-            point.positions = self.current_joint_state
-            point.time_from_start.sec = duration
+            # Send joint command
+            self.piper.JointControl(*self.current_joint_state)
 
-            msg.points.append(point)
-
-            self.arm_pub.publish(msg)
-
-            time.sleep(duration + 1)
+            time.sleep(0.05)
+            
